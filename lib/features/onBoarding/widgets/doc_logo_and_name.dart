29447bf67c5174cf725570f93d492d4b569459b8
Nadeen Radwan <nadeenradwan0@gmail.com>
2023-12-13 13:31:22 +0200

    edit name for doc doc

--- lib/features/onBoarding/widgets/doc_logo_and_name.dart
+++ lib/features/onBoarding/widgets/doc_logo_and_name.dart
@@ -15,7 +15,7 @@ class DocLogoAndName extends StatelessWidget {
         SvgPicture.asset('assets/svgs/drdoc_logo.svg'),
         SizedBox(width: 10.w),
         Text(
-          'Dr. Doc',
+          'Doc Doc',
           style: TextStyles.font24Black700Weight,
         )
       ],
